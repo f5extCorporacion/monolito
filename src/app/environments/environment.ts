1ce444f93c0084e344fb30fcbb21dc5adb5bda03
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2026-06-16 13:07:45 -0500

    Change API URL to localhost for development
    Updated the API URL for development environment.

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,12 +1,8 @@
 // src/environments/environment.ts (desarrollo)
 import { Environment } from './environment.interface';
 export const environment: Environment = {
-  production: true,
-
-  // URL de tu aplicación en producción
-  apiUrl: 'https://monolito-flame.vercel.app',
-
-  // Supabase
+ production: true, // Si es desarrollo, cámbialo a false
+  apiUrl: 'http://localhost:4200',
   supabaseUrl: 'https://sgpalbtterlwbwuqdnhy.supabase.co',
   supabaseKey: 'sb_publishable_Mswq34K1u1elx4AvDPTQSQ_rmahoTKC' 
 };
