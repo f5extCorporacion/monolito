bba6daa9b199ba554abff2f56426d9bbdd3b4bde
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2026-06-16 11:41:59 -0500

    Update environment.ts
    version production

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,7 +2,7 @@
 import { Environment } from './environment.interface';
 
 export const environment: Environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://monolito-flame.vercel.app',
   NEXT_PUBLIC_SUPABASE_URL:'https://sgpalbtterlwbwuqdnhy.supabase.co',
   NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY:'sb_publishable_Mswq34K1u1elx4AvDPTQSQ_rmahoTKC'
