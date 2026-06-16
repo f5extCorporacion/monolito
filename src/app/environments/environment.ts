8d24fa8d72b679d34647c3b14050894a7a7a6948
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2026-06-16 13:09:54 -0500

    Update environment.ts
    ahora si los datos estan bien

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,7 +2,7 @@
 import { Environment } from './environment.interface';
 export const environment: Environment = {
  production: true, // Si es desarrollo, cámbialo a false
-  apiUrl: 'http://localhost:4200',
+  apiUrl: 'https://monolito-flame.vercel.app',
   supabaseUrl: 'https://sgpalbtterlwbwuqdnhy.supabase.co',
   supabaseKey: 'sb_publishable_Mswq34K1u1elx4AvDPTQSQ_rmahoTKC' 
 };
