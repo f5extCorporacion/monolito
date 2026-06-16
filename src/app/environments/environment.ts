58a21cec0ce4fc5bdce378c00e10b29dbdab018a
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2026-06-16 11:57:10 -0500

    Update environment.ts

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,9 +2,8 @@
 import { Environment } from './environment.interface';
 
 export const environment: Environment = {
-  production: true,
-  apiUrl: 'https://sgpalbtterlwbwuqdnhy.supabase.co/rest/v1',
-  NEXT_PUBLIC_SUPABASE_URL:'https://sgpalbtterlwbwuqdnhy.supabase.co',
-  NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY:'sb_publishable_Mswq34K1u1elx4AvDPTQSQ_rmahoTKC'
-
+  production: true, // Si es desarrollo, cámbialo a false
+  apiUrl: 'http://localhost:4200',
+  supabaseUrl: 'https://sgpalbtterlwbwuqdnhy.supabase.co',
+  supabaseKey: 'sb_publishable_Mswq34K1u1elx4AvDPTQSQ_rmahoTKC' 
 };
